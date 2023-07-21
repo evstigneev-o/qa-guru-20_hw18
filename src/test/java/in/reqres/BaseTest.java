9ea20b88f0ec582aebdd563f702246e04ce9c56a
Oleg Evstigneev <o.evstigneev@citydrive.ru>
2023-07-21 19:54:39 +0300

    add some tests

---
+++ src/test/java/in/reqres/BaseTest.java
@@ -0,0 +1,18 @@
+package in.reqres;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+    static final String BASE_URL = "https://reqres.in/";
+    static final String API_PATH = "api";
+
+    @BeforeAll
+    public static void setUp(){
+        RestAssured.baseURI = BASE_URL;
+        RestAssured.basePath = API_PATH;
+        RestAssured.filters(new RequestLoggingFilter(),new ResponseLoggingFilter());
+    }
+}
---
+++ src/test/java/in/reqres/models/RegisterRequest.java
@@ -0,0 +1,13 @@
+package in.reqres.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RegisterRequest {
+    private String email;
+    private String password;
+}
